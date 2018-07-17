b6b8c395a1c03ce83922349cd7a2cec55ef456bb
whoozit.whatsit@gmail.com <whoozit.whatsit@gmail.com>
2018-07-17 22:45:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,7 +20,10 @@ function destructivelyRemoveFirstKitten() {
 
 function appendKitten(name) {
   return  [...kittens, name];
-  
+}
+
+function prependKitten(name) {
+  return [name, ...kittens];
 }
 
 
