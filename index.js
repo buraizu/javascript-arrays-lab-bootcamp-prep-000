b9de8946fa45f0f6e8a8069c1518c409d7f56940
whoozit.whatsit@gmail.com <whoozit.whatsit@gmail.com>
2018-07-17 22:15:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Add your functions and code here
+
+var kittens = ['Milo', 'Otis', 'Garfield'];
+
+function destructivelyAppendKitten() {
+  kittens.push('Ralph');
+}
+
+function destructivelyPrependKitten() {
+  kittens.unshift('Bob');
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop();
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift();
+}
+
+function appendKitten() {
+  kittens.push('Broom');
+}
